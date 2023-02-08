33c585cc3381cc9dcab595d44c0790c364491c27
L-K-Chng <lipkuang02@gmail.com>
2023-02-08 12:03:18 +0800

    first commit. Added quick workout and focus group exercises.

--- FitnessLogos.java
+++ FitnessLogos.java
@@ -1,3 +1,89 @@
-public class FitnessLogos {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class FitnessLogos {
+    //Data Storage
+    private static final ArrayList<String> listOfExercises = new ArrayList<>();
+
+    //Core
+    static final String SIT_UPS = "Sit Ups";
+    static final String PLANK = "Plank";
+    static final String SIDE_PLANK = "Side Plank";
+    static final String CRUNCHES = "Crunches";
+
+    //Arms
+    static final String PUSH_UPS = "Push Ups";
+    static final String WIDE_PUSHUPS = "Wide Push Ups";
+    static final String PLANK_SHOULDER_TAPS = "Plank Shoulder Taps";
+    static final String PULL_UPS = "Pull Ups";
+    static final String DIAMOND_PUSHUPS = "Diamond Push Ups";
+
+    //Legs
+    static final String SQUATS = "Squats";
+    static final String FORWARD_LUNGES = "Forward Lunges";
+    static final String REAR_LUNGES = "Rear Lunges";
+    static final String JUMPING_JACKS = "Jumping Jacks";
+    static final String PISTOL_SQUATS = "Pistol Squats";
+    static final String HIGH_KNEES = "High Knees";
+
+    //Everything
+    static final String BURPEES = "Burpees";
+    static final String MOUNTAIN_CLIMBERS = "Mountain Climbers";
+
+    public static void quickWorkout() {
+        List<String> exerciseList = Arrays.asList(SQUATS, WIDE_PUSHUPS,CRUNCHES, BURPEES, HIGH_KNEES, PLANK_SHOULDER_TAPS, SIDE_PLANK, MOUNTAIN_CLIMBERS);
+        listOfExercises.addAll(exerciseList);
+
+        String instructions = "Do this set of exercises for a quick pump! " +
+                "Make sure to stick to a Work-Rest Cycle of 60S-30S for best results!";
+        System.out.println(instructions);
+
+        for (int i = 0; i < listOfExercises.size(); i++) {
+            System.out.println(i+1 + ". " + listOfExercises.get(i));
+        }
+        listOfExercises.clear();
+    }
+
+    public static void focusGroupArms() {
+        List<String> exerciseList = Arrays.asList(PUSH_UPS,PLANK_SHOULDER_TAPS, WIDE_PUSHUPS, PULL_UPS, DIAMOND_PUSHUPS);
+        listOfExercises.addAll(exerciseList);
+
+        String instructions = "Try this set of exercises for a killer arm workout! " +
+                "Make sure to stick to a Work-Rest Cycle of 60S-30S for best results!";
+        System.out.println(instructions);
+
+        for (int i = 0; i < listOfExercises.size(); i++) {
+            System.out.println(i+1 + ". " + listOfExercises.get(i));
+        }
+        listOfExercises.clear();
+    }
+
+    public static void focusGroupLegs() {
+        List<String> exerciseList = Arrays.asList(SQUATS, FORWARD_LUNGES, JUMPING_JACKS, PISTOL_SQUATS, REAR_LUNGES);
+        listOfExercises.addAll(exerciseList);
+
+        String instructions = "Try this set of exercises for a killer leg workout! " +
+                "Make sure to stick to a Work-Rest Cycle of 60S-30S for best results!";
+        System.out.println(instructions);
+
+        for (int i = 0; i < listOfExercises.size(); i++) {
+            System.out.println(i+1 + ". " + listOfExercises.get(i));
+        }
+        listOfExercises.clear();
+    }
+
+    public static void focusGroupCore() {
+        List<String> exerciseList = Arrays.asList(SIT_UPS, BURPEES, CRUNCHES, PLANK, SIDE_PLANK);
+        listOfExercises.addAll(exerciseList);
+
+        String instructions = "Try this set of exercises for a killer core workout! " +
+                "Make sure to stick to a Work-Rest Cycle of 60S-30S for best results!";
+        System.out.println(instructions);
+
+        for (int i = 0; i < listOfExercises.size(); i++) {
+            System.out.println(i+1 + ". " + listOfExercises.get(i));
+        }
+        listOfExercises.clear();
+    }
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class Main {
 
+    //Commands
+    static final String COMMAND_QUICK_WORKOUT = "Quick Workout";
+    static final String COMMAND_FOCUS_GROUP = "Focus Group";
+    static final String COMMAND_END = "End";
+    static final String COMMAND_ARMS = "Arms";
+    static final String COMMAND_LEGS = "Legs";
+    static final String COMMAND_CORE = "Core";
+
     public static void main(String[] args) {
         System.out.println("Welcome to FITNESS DUKE");
         Scanner currentScanner = new Scanner(System.in);
@@ -14,24 +22,38 @@ public class Main {
         System.out.println("Please type in any of the following commands:");
         System.out.println("Quick Workout : Quickly generates a recommended workout according to your needs");
         System.out.println("Focus Group : Exercise a particular muscle group");
-        System.out.println("");
+        System.out.println("End : Done for the day and want to exit this program (Highly not recommended)");
         boolean workoutChosen = false;
+
         while (!workoutChosen) {
             String typeOfWorkout = currentScanner.nextLine();
             switch (typeOfWorkout) {
-                case "Quick Workout":
-                    System.out.println("HABLAHABLAHABLA HERES A QUICK WORKOUT");
-                    workoutChosen = true;
-                    break;
-                case "Focus Group":
-                    System.out.println("HABLA FOCUS GROUP LALALA");
-                    workoutChosen = true;
-                    break;
-                default:
-                    System.out.println("Walao give an appropriate command leh");
+            case COMMAND_QUICK_WORKOUT:
+                System.out.println("HABLAHABLAHABLA HERES A QUICK WORKOUT");
+                FitnessLogos.quickWorkout();
+                break;
+            case COMMAND_FOCUS_GROUP:
+                System.out.println("Which particular muscle group would you like to work on? (You can choose from either Arms, Legs or Core)");
+                String input;
+                input = currentScanner.nextLine();
+                if (input.equals(COMMAND_ARMS)) {
+                    FitnessLogos.focusGroupArms();
+                } else if (input.equals(COMMAND_LEGS)) {
+                    FitnessLogos.focusGroupLegs();
+                } else if (input.equals(COMMAND_CORE)) {
+                    FitnessLogos.focusGroupCore();
+                } else {
+                    System.out.println("Please key in a valid muscle group! (Arms/Legs/Core)");
+                }
+                break;
+            case COMMAND_END:
+                System.out.println("Hope you had a great workout today, goodbye!");
+                workoutChosen = true;
+                break;
+            default:
+                System.out.println("Please give an appropriate command! (Quick Workout/Focus Group/End)");
             }
         }
-
         currentScanner.close();
     }
 }
